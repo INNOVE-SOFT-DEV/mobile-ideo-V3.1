6aeaccf0a98b1103bf703817763a596fa485224e
MohamedAliFaidi <mouhammedalifaidi@icloud.com>
2025-10-31 12:04:44 +0100

    modified:   src/app/pages/tickets/add-ticket/add-ticket.page.html 	modified:   src/app/pages/tickets/add-ticket/add-ticket.page.ts 	modified:   src/environments/environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
-  // ws_api_url: "wss://api.ideogroupe.com/cable",
-  // urlAPI: "https://api.ideogroupe.com/",
-  // url_web: "https://backend.ideogroupe.com/",
+  ws_api_url: "wss://api.ideogroupe.com/cable",
+  urlAPI: "https://api.ideogroupe.com/",
+  url_web: "https://backend.ideogroupe.com/",
   // mattermost_ws_url: "wss://chat.ideogroupe.com/api/v4/websocket",
   // matter_https_url: "https://chat.ideogroupe.com/api/v4/",
 
-  ws_api_url: "wss://apideo.webo.tn/cable",
-  urlAPI: "https://apideo.webo.tn/",
-  url_web: "https://ideo.webo.tn/",
+  // ws_api_url: "wss://apideo.webo.tn/cable",
+  // urlAPI: "https://apideo.webo.tn/",
+  // url_web: "https://ideo.webo.tn/",
 
   newApiUrl: "https://9c05716e54b4.ngrok-free.app/api/v1/",
 
