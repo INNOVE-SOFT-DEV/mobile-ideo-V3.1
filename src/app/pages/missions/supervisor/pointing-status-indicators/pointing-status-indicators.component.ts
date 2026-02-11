83becb49c021ad57e64df1c81bf4eb1660f6ef78
Ftiti <fadhel.dev@gmail.com>
2026-02-11 15:04:21 +0100

    fixed pointage

--- src/app/pages/missions/supervisor/pointing-status-indicators/pointing-status-indicators.component.ts
+++ src/app/pages/missions/supervisor/pointing-status-indicators/pointing-status-indicators.component.ts
@@ -1,5 +1,5 @@
 import {CommonModule} from "@angular/common";
-import {Component, ElementRef, Input, OnInit} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
 
 @Component({
   selector: "app-pointing-status-indicators",
@@ -10,23 +10,9 @@ import {Component, ElementRef, Input, OnInit} from "@angular/core";
 export class PointingStatusIndicatorsComponent implements OnInit {
   @Input() agent: any;
 
-  constructor(private el: ElementRef) {}
+  constructor() {}
 
   ngOnInit() {
     // console.log(this.agent);
-  }
-  ngAfterViewInit() {
-    setTimeout(() => {
-      const blocks: HTMLElement[] = Array.from(this.el.nativeElement.querySelectorAll(".custom-block"));
-
-      blocks.forEach((block, index) => {
-        setTimeout(() => {
-          block.classList.add("animate__animated", "animate__fadeInUp");
-          block.style.opacity = "1";
-          block.style.transform = "translateY(0)";
-          block.style.animationDuration = "500ms";
-        }, index * 100);
-      });
-    }, 200);
   }
 }
